4ea5ba6592d77fe903736567bc7cbeeaa450f1e3
Matthew John Ragas <mjrag310@hotmail.com>
2020-09-24 16:30:02 -0500

    Unity Physics Assessment

---
+++ Assets/ActivateLaunch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateLaunch : MonoBehaviour
+{
+    public HingeJoint HingeJointR = new HingeJoint();
+    public HingeJoint HingeJointL = new HingeJoint();
+    private 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            HingeJointL.useMotor = true;
+            HingeJointR.useMotor = true;
+        }
+    }
+}
---
+++ Assets/RagdollTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Ragdoll ragdoll = other.gameObject.GetComponentInParent<Ragdoll>();
+        if (ragdoll != null)
+            ragdoll.RagdollOn = true;
+    }
+}
